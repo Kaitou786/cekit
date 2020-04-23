9dea0a27c27ca3f532a58da1a461af63c3713c34
Tarun khandelwal <tkhandelwal32@gmail.com>
2020-04-24 00:13:34 +0530

    working all

--- cekit/modules/go/install.sh
+++ cekit/modules/go/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 tar -C /usr/local/ -xf  /tmp/artifacts/go1.13.linux-amd64.tar.gz
-
+yum-config-manager  --enable rhel-7-server-extras-rpms  #enable rhel-7-server-extras-rpms repo for the image
--- cekit/modules/podman/soft-link.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh 
-set -e
-ln -s /usr/bin/podman   /usr/bin/docker
